80e6218a281b51e125a01f3ad9f6095d33e6a9e5
김형운 <guddns@gmail.com>
2013-07-09 03:24:11 +0900

    싱글톤 매크로 추가

--- HFramework/Core/HF_Singleton.h
+++ HFramework/Core/HF_Singleton.h
@@ -22,3 +22,97 @@
         }); \
         return __singleton__; \
     }
+
+
+////////////////////////////////////////////////////////////////////////
+// 클래스를 싱글톤으로 생성하는 매크로
+
+#define SYNTHESIZE_SINGLETON_FOR_CLASS(classname) \
+\
+static classname *shared##classname = nil; \
+\
++ (classname *)shared##classname \
+{ \
+@synchronized(self) \
+{ \
+if (shared##classname == nil) \
+{ \
+shared##classname = [[self alloc] init]; \
+} \
+} \
+\
+return shared##classname; \
+} \
+\
++ (id)allocWithZone:(NSZone *)zone \
+{ \
+@synchronized(self) \
+{ \
+if (shared##classname == nil) \
+{ \
+shared##classname = [super allocWithZone:zone]; \
+return shared##classname; \
+} \
+} \
+\
+return nil; \
+} \
+\
+- (id)copyWithZone:(NSZone *)zone \
+{ \
+return self; \
+} \
+\
+- (id)retain \
+{ \
+return self; \
+} \
+\
+- (NSUInteger)retainCount \
+{ \
+return NSUIntegerMax; \
+} \
+\
+- (id)autorelease \
+{ \
+return self; \
+}
+
+////////////////////////////////////////////////////////////////////////
+// XIB 가 있는 ViewController 의 getter 를 추가하는 매크로
+
+#define NEW_INSTANTIATION_XIB(type) \
+\
+- (type *)create##type { \
+type *vc = [[[type alloc] initWithNibName:[NSString stringWithFormat:@"%@", [type class]] bundle:nil] autorelease]; \
+NSLog(@"%@ is instantiation.", vc); \
+return vc; \
+}
+
+
+////////////////////////////////////////////////////////////////////////
+// XIB 가 있는 ViewController 의 getter 를 추가하는 매크로
+
+#define LAZY_INSTANTIATION_XIB(type, name) \
+\
+- (type *)name { \
+if (_##name == nil) { \
+_##name = [[type alloc] initWithNibName:[NSString stringWithFormat:@"%@", [type class]] bundle:nil]; \
+NSLog(@"%@ is instantiation.", _##name); \
+} \
+return _##name; \
+}
+
+
+////////////////////////////////////////////////////////////////////////
+// getter 를 추가하는 매크로
+
+#define LAZY_INSTANTIATION(type, name) \
+\
+- (type *)name { \
+if (!_##name) { \
+_##name = [[type alloc] init]; \
+NSLog(@"%@ is instantiation.", _##name); \
+} \
+return _##name; \
+}
