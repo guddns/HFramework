d20c394b8e11231b5688e773a37ca46cf059fdb1
HyeongUn Kim <guddns@gmail.com>
2013-08-01 15:37:22 +0900

    no message

---
+++ HFramework/Additions/View/UIImage+Additions.h
@@ -0,0 +1,13 @@
+//
+//  UIImage+Additions.h
+//  HFramework
+//
+//  Created by hug on 13. 8. 1..
+//  Copyright (c) 2013년 hug. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (Additions)
+
+@end
---
+++ HFramework/Additions/View/UIImage+Additions.m
@@ -0,0 +1,13 @@
+//
+//  UIImage+Additions.m
+//  HFramework
+//
+//  Created by hug on 13. 8. 1..
+//  Copyright (c) 2013년 hug. All rights reserved.
+//
+
+#import "UIImage+Additions.h"
+
+@implementation UIImage (Additions)
+
+@end
