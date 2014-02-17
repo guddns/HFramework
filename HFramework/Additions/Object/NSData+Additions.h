85140841b0abca6e4ca3e91e615e1ddbe1ce5968
HyeongUn Kim <guddns@gmail.com>
2014-02-17 11:14:33 +0900

    no message

---
+++ HFramework/Additions/Object/NSData+Additions.h
@@ -0,0 +1,15 @@
+//
+//  NSData+Additions.h
+//  HFramework
+//
+//  Created by hug on 2014. 2. 17..
+//  Copyright (c) 2014년 hug. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSData (Additions)
+
++ (NSData *)base64DataFromString:(NSString *)string;
+
+@end
